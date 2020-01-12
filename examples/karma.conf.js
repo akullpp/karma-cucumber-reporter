e629e8c101ccebf87c7256302147f9b122b3ea58
Andreas Kull <akullpp@gmail.com>
2020-01-12 21:24:17 +0100

    Update dependencies to latest

--- examples/karma.conf.js
+++ examples/karma.conf.js
@@ -1,17 +1,15 @@
-'use strict';
+"use strict";
 
 module.exports = function(config) {
   config.set({
     singleRun: true,
-    files: [
-        'test/**/*'
-    ],
-    frameworks: ['mocha', 'chai'],
-    browsers: ['Chrome'],
-    reporters: ['cucumber'],
+    files: ["test/**/*"],
+    frameworks: ["mocha", "chai"],
+    browsers: ["Chrome"],
+    reporters: ["cucumber"],
     cucumberReporter: {
-        out: 'cucumber.json',
-        prefix: 'XYZ'
+      out: "report/cucumber.json",
+      prefix: "XYZ"
     }
   });
 };
